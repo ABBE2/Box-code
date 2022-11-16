ad94219c895fa3722c12903f19c9b4fd4601acba
Admin <Admin@TEB123014>
2022-11-16 10:02:16 +0100

    Add project files.

---
+++ Box code/Box_code.py
@@ -0,0 +1,8 @@
+ruta = " "*50
+print(ruta,'----------------------')
+print(ruta,'|   Numeriska fel    |')
+print(ruta,'|                    |')
+print(ruta,'|   Albin Andersson  |')
+print(ruta,'|                    |')
+print(ruta,'|     2022-11-16     |')
+print(ruta,'----------------------')
